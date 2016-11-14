d3800078ffaa452c59c0a64b4e1689816bb0d4bf
Melaniia Hulianovych <mhul@softserveinc.com>
2016-11-14 11:05:04 +0200

    Initial Commit

---
+++ Shops/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shops
+//
+//  Created by Melaniia Hulianovych on 11/14/16.
+//  Copyright © 2016 Melaniia Hulianovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
